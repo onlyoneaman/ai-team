5407bd51592b0e5f6ce36d8d65676835d2f680f1
onlyoneaman <2000.aman.sinha@gmail.com>
2026-02-01 23:54:37 +0530

    chore: use PORT env variable for uvicorn server

--- backend/main.py
+++ backend/main.py
@@ -41,5 +41,8 @@ app.include_router(api_router)
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
